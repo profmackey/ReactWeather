fe7277f0548d8bfabd6c3454a8ddeb928217b1b5
Prof Mackey <profmackey@gmail.com>
2017-02-22 15:46:44 -0500

    Style About and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,8 +11,23 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to About Page!</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application built on React for the Udemy Course.
+      </p>
+      <p>
+        Here are some of the development tools utilized:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This is the
+            JavaScript user interface library used as a foundation.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - This is
+            the tool used to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   )
 };
