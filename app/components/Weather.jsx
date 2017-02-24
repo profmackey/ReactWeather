c175239dee94c6698ba9151f55a1e7b1b16bdd5c
Prof Mackey <profmackey@gmail.com>
2017-02-24 14:36:14 -0500

    Add Nav Search Support

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -12,9 +12,12 @@ var Weather = React.createClass({
   },
   handleSearch: function (location) {
     var that = this;
+
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
 
     openWeatherMap.getTemp(location).then(function (temp) {
@@ -29,6 +32,22 @@ var Weather = React.createClass({
         errorMessage: e.message
       });
     });
+  },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
   },
   render: function () {
     var {isLoading, temp, location, errorMessage} = this.state;
